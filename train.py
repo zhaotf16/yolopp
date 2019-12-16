4e5011d38328d8f6ea099257599a27e305fb42fc
zhaotf16 <zhaotianfang2010@163.com>
2019-12-16 19:12:43 +0800

    a local debug version

--- train.py
+++ train.py
@@ -24,25 +24,43 @@ def train():
     star = starHelper.read_all_star(label_path)
     
     array = dataLoader.mrc2array(mrc, image_size=1024)
-    array = np.expand_dims(array, axis=1)
-    label = dataLoader.star2label(star, 1024)
-
-    batchsize = 1
-    epochs = 10
-    learning_rate = 0.1
+    label = dataLoader.star2label(star, 1024, grid_size=64, 
+        particle_size=(110/7420*1024, 110/7676*1024),
+    )
+    
+    batchsize = 2
+    epochs = 300
+    learning_rate = 0.001
     net = cn.PhosaurusNet()
     optimizer = tf.optimizers.Adam(learning_rate=learning_rate)
 
+    #debug:
+    #array = np.expand_dims(array[0, ...], axis=0)
+    #batchsize = 1
+
     for e in range(epochs):
         batch_num = array.shape[0] // batchsize
+        total_loss = 0
         for i in range(batch_num):
-            x = array[i+batchsize-1, ...]
-            y_true = label[i+batchsize-1, ...]
+            index = i * batchsize
+            x = array[index:index+batchsize, ...]
+            y_true = label[index:index+batchsize, ...]
             with tf.GradientTape() as tape:
                 y_pred = net(x, training=True)
-                loss = cn.yolo_loss(y_pred, y_true)
-                loss = tf.reduce_mean(loss)
-            print("epoch: %d\tbatch: %d\t loss: %f" % (e+1, i+1, loss))
+                #loss = cn.yolo_loss(y_pred, y_true)
+                #loss = tf.reduce_mean(loss)
+                #debug:
+                xy_loss, wh_loss, obj_loss = cn.yolo_loss(y_pred, y_true)
+                xy_loss = tf.reduce_mean(xy_loss)
+                wh_loss = tf.reduce_mean(wh_loss)
+                obj_loss = tf.reduce_mean(obj_loss)
+                loss = xy_loss + wh_loss + obj_loss
+                total_loss += loss
+            #print("epoch: %d\tbatch: %d\tloss: %f" % (e+1, i+1, loss))
+            #average_loss = total_loss / batch_num
+            #print("epoch: %d\tloss:%f" %(e, average_loss))
+            print("epoch: %d\tbatch: %d\txy_loss: %f\twh_loss: %f\tobj_loss: %f\tloss: %f" % 
+            (e+1, i+1, xy_loss, wh_loss, obj_loss, loss))
             grads = tape.gradient(loss, net.trainable_variables)
             optimizer.apply_gradients(grads_and_vars=zip(grads, net.trainable_variables))
     
