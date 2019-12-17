1dfca441e05549289b3c2c58e849150091cddf10
zhaotf16 <zhaotianfang2010@163.com>
2019-12-17 17:45:39 +0800

    over-fit found

--- pick.py
+++ pick.py
@@ -33,16 +33,30 @@ def pick(argv):
     net = cn.PhosaurusNet()
 
     #debug:
-    #array = np.expand_dims(array[0, ...], axis=0)
-    #batchsize = 1
+    star = starHelper.read_all_star("../dataset/EMPIAR-10025/processed/test_label")
+    label = preprocess.star2label(star, 1024, grid_size=64, 
+        particle_size=(110/7420*1024, 110/7676*1024),
+    )
+
     net.load_weights('yolopp_weights/')
     batch_num = np.shape(array)[0] // batchsize
-        
+
     for i in range(batch_num):
         index = i * batchsize
         x = array[index:index+batchsize, ...]
+        y_true = label[index:index+batchsize, ...]
         y_pred = net(x, training=False)
-        bbox, score = cn.yolo_head(y_pred)
+        #bbox, score = cn.yolo_head(y_pred)
+        #boxes = cn.non_max_suppression(bbox, score, 0.5)
+        #score = tf.cast(score>0.2, tf.float32)
+        #print(tf.reduce_sum(score))
+        xy_loss, wh_loss, obj_loss = cn.yolo_loss(y_pred, y_true)
+        xy_loss = tf.reduce_mean(xy_loss)
+        wh_loss = tf.reduce_mean(wh_loss)
+        obj_loss = tf.reduce_mean(obj_loss)
+        loss = xy_loss + wh_loss + obj_loss
+        print("batch: %d\txy_loss: %f\twh_loss: %f\tobj_loss: %f\tloss: %f" % 
+        (i+1, xy_loss, wh_loss, obj_loss, loss))
     #format of output files is STAR
     stars = []
 
